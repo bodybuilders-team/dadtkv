0c695a52d8399279261713815078a0e83f17cac2
André Jesus <andre.jesus.pilar@gmail.com>
2023-09-22 14:29:52 +0100

    Added projects in solution: client, transaction server and lease manager server.

---
+++ DADTKVClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DADTKVClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DADTKVTransactionServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DADTKVTransactionServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
