4fe003d7329f9070e28b8f226c58a50f7af5ff9f
André Jesus <andre.jesus.pilar@gmail.com>
2023-09-23 21:15:29 +0100

    TransactionServer grpc server initialization added.

--- DADTKVTransactionServer/Program.cs
+++ DADTKVTransactionServer/Program.cs
@@ -1,10 +1,53 @@
 ﻿namespace DADTKVTransactionServer
 {
+    // TODO: Needs to be server and client fo the state updade service
+    // TODO: Needs to be Client of the lease service
+
+
+    public class ServerService : DADTKVService.DADTKVServiceBase
+    {
+        
+        public ServerService()
+        {
+        }
+
+        public override Task<TxSubmitReply> TxSubmit(TxSubmitRequest request, ServerCallContext context)
+        {
+            // ...
+        }
+
+        public override Task<StatusReply> Status(StatusRequest request, ServerCallContext context)
+        {
+            // ...
+        }
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // Set up the gRPC server
+            const int port = 1001;
+            const string hostname = "localhost";
+            ServerPort serverPort;
+
+            serverPort = new ServerPort(hostname, port, ServerCredentials.Insecure);
+
+            Server server = new Server
+            {
+                Services = { ChatServerService.BindService(new ServerService()) },
+                Ports = { serverPort }
+            };
+
+            // Start the gRPC server
+            server.Start();
+            Console.WriteLine($"Server listening on port {port}");
+
+            // Configuring HTTP for client connections in Register method
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            // Keep the server running indefinitely
+            while (true) ;
         }
     }
 }
