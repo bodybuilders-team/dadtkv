5bdc95fe643e2b4d1d4dc47317719a9c491e5d74
Nyck <nyckbrandao1236@gmail.com>
2023-09-24 22:13:28 +0100

    Fixed DADTKVCore configuration file parsing.

--- DADTKVCore/Program.cs
+++ DADTKVCore/Program.cs
@@ -7,7 +7,8 @@
         private static void Main(string[] args)
         {
             // Read the system configuration file
-            const string configurationFile = "system_config.txt"; // args[0];
+            string configurationFile = Path.Combine(Environment.CurrentDirectory,
+                "/IST/1st Semester/DAD/ist-meic-dad-g05/DADTKVCore/configurations/configuration_sample.txt"); // args[0];
             var configuration = ReadSystemConfiguration(configurationFile);
 
             // Start DADTKV servers (Transaction Managers, Lease Managers)
@@ -37,9 +38,13 @@
                         var command = parts[0];
                         var parameters = parts.Skip(1).ToArray();
 
+                        List<ProcessInfo>? serverProcesses = null;
+
                         // Process different commands from the configuration file
                         switch (command)
                         {
+                            case "#":
+                                continue;
                             case "P": // Process identifier and role (Server or Client)
                                 var process = new ProcessInfo
                                 {
@@ -65,8 +70,16 @@
 
                             case "F": // Suspected nodes during time slots
                                 var slotNumber = int.Parse(parameters[0]);
-                                var suspectedNodeIDs = parameters.Skip(1).ToList();
-                                systemConfig.SuspectedNodes[slotNumber] = suspectedNodeIDs;
+                                serverProcesses ??= systemConfig.ServerProcesses;
+                                for (var i = 0; i < serverProcesses.Count; i++)
+                                {
+                                    serverProcesses[i].SlotStatus[slotNumber] = parameters[1 + i];
+                                }
+
+                                var suspicions = parameters.Skip(1 + serverProcesses.Count).ToList();
+
+                                systemConfig.Suspicions[slotNumber] = suspicions.Select(s => s.Trim('(', ')'))
+                                    .Select(s => new Tuple<string, string>(s.Split(',')[0], s.Split(',')[1])).ToList();
                                 break;
                         }
                     }
@@ -84,7 +97,7 @@
         private static void StartServers(SystemConfiguration? config)
         {
             // Start server processes based on configuration
-            foreach (var process in config?.Processes.Where(process => process.Role == "Server")!)
+            foreach (var process in config?.Processes.Where(process => process.Role is "T" or "L")!)
             {
                 // Start the server process using process.ID and process.URL
                 Console.WriteLine($"Starting {process.Role} {process.ID} at {process.URL}");
@@ -105,16 +118,25 @@
             public string ID { get; set; }
             public string Role { get; set; }
             public string URL { get; set; }
+            public Dictionary<int, string> SlotStatus { get; } = new Dictionary<int, string>();
         }
 
         // Class to represent the system configuration
         public class SystemConfiguration
         {
             public List<ProcessInfo> Processes { get; } = new List<ProcessInfo>();
+
+            public List<ProcessInfo> ServerProcesses
+            {
+                get { return Processes.FindAll(p => p.Role is "T" or "L"); }
+            }
+
             public int Slots { get; set; }
             public int Duration { get; set; }
             public DateTime WallTime { get; set; }
-            public Dictionary<int, List<string>> SuspectedNodes { get; } = new Dictionary<int, List<string>>();
+
+            public Dictionary<int, List<Tuple<string, string>>> Suspicions { get; } =
+                new Dictionary<int, List<Tuple<string, string>>>();
         }
     }
 }
